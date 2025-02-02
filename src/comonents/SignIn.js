c0ba95d37d5e9a84ba46147da1feeebbee21d335
Srijan1202 <srijan.srivastava1202@gmail.com>
2025-02-02 23:56:53 +0530

    SigIN page update

--- src/comonents/SignIn.js
+++ src/comonents/SignIn.js
@@ -2,14 +2,15 @@ import React from "react";
 
 const SignIn = () => {
   return (
-    <div >
-        <div className='bg-black'>
-      <img className="opacity-50 w-[100vw] h-[100vh]"
-        src="https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_small.jpg"
-        alt=""
-      /
-      >
-        </div>
+    <div className="bg-black">
+      <div>
+        <img
+          className="opacity-50 w-[100vw] h-[100vh]"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/fb5cb900-0cb6-4728-beb5-579b9af98fdd/web/IN-en-20250127-TRIFECTA-perspective_cf66f5a3-d894-4185-9106-5f45502fc387_small.jpg"
+          alt=""
+        />
+        <div className="h-20 w-20 bg-black absolute"></div>
+      </div>
     </div>
   );
 };
