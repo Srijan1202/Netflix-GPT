349a074bc4d69d51a7db69835beab3a527936d39
Srijan1202 <srijan.srivastava1202@gmail.com>
2025-02-05 23:59:36 +0530

    added nav bar

--- src/comonents/Header.js
+++ src/comonents/Header.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import img from '../utils/pngwing.com.png'
 
 const Header = () => {
   return (
     <div>
-      header
+      <img src={img} alt=""  className='w-48 ml-40'/>
     </div>
   )
 }
