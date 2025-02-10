10d28b7fc7b6ecb650b2dadc2a977eac361f9ffc
Srijan1202 <srijan.srivastava1202@gmail.com>
2025-02-10 23:38:49 +0530

    changes to browse page

--- src/comonents/Maincontainer.js
+++ src/comonents/Maincontainer.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Videocontainer from './Videocontainer'
 import Titlecontainer from './Titlecontainer'
+import { useSelector } from 'react-redux'
 
 const Maincontainer = () => {
+    const data = useSelector(state => state.movies);
+    console.log(data[0]?.original_title);
   return (
     <div>
-      <Titlecontainer/>
+      <Titlecontainer title={data[0]?.original_title} discription={data[0]?.overview} />
       <Videocontainer/>
     </div>
   )
--- src/comonents/Titlecontainer.js
+++ src/comonents/Titlecontainer.js
@@ -1,14 +1,12 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-import { data } from 'react-router-dom'
 
-const Titlecontainer = () => {
-    data = useSelector(state => state.data)
+const Titlecontainer = ({title,discription}) => {
+    
 
   return (
     <div>
-      <h1 className='text-white'>Title</h1>
-      <div className='text-white'>discription</div>
+      <h1 className='text-white'>{title}</h1>
+      <div className='text-white'>{discription}</div>
       <button className='text-black font-bold px-20 py-3 bg-white bg-opacity-70 m-2 rounded-md'>play</button>
       <button className='text-white px-20 py-3 bg-slate-700 bg-opacity-30 m-2 rounded-md'>more info</button>
     </div>
