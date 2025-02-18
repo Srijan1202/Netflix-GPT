2d3f2c3b44e2482ddb64c1efe9afc8576a6b0241
Srijan1202 <srijan.srivastava1202@gmail.com>
2025-02-19 00:06:51 +0530

    multilanguage fixed

--- src/comonents/Gptsearch.js
+++ src/comonents/Gptsearch.js
@@ -1,19 +1,25 @@
-import React from 'react'
-import lang from '../utils/languageconstants'
-import { useSelector } from 'react-redux'
+import React from "react";
+import lang from "../utils/languageconstants";
+import { useSelector } from "react-redux";
 
 const Gptsearch = () => {
-
-  const langu= useSelector((state) => state.config.language);
-  console.log(langu)
+  const langu = useSelector((state) => state.language.language);
+  const currentLang = lang[langu] || lang["en"];
+  console.log(langu);
   return (
-    <div className="flex justify-center items-center bg-red-600 h-20 w-full rounded-t-lg bg-opacity-80"> 
+    <div className="flex justify-center items-center bg-red-600 h-20 w-full rounded-t-lg bg-opacity-80">
       <form action="submit">
-        <input type="text" placeholder={lang[langu].placeholder} className='text-center p-3 w-2/3 bg-black bg-opacity-80 rounded-md text-white'/>
-        <button className='bg-black w-24 h-12 ml-1 rounded-md text-white bg-opacity-90'>{lang[langu].search}</button> 
+        <input
+          type="text"
+          placeholder={currentLang.placeholder}
+          className="text-center p-3 w-2/3 bg-black bg-opacity-80 rounded-md text-white"
+        />
+        <button className="bg-black w-24 h-12 ml-1 rounded-md text-white bg-opacity-90">
+          {currentLang.search}
+        </button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Gptsearch
+export default Gptsearch;
--- src/utils/Appstore.js
+++ src/utils/Appstore.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import userReaducer from "./userSlice";
 import moviesReducer from "./movieSlice";
 import toggleReducer from "./toggleSlice";
+import languageReducer from "./languageSlice";
 
 const Appstore = configureStore({
   reducer: {
     user: userReaducer,
     movies: moviesReducer,
     toggle: toggleReducer,
+    language: languageReducer,
   },
 });
 
